f8e381d9153b7e6bb9aa260192b147b09a8dce11
Megan Hardman <meganlillyhardman@gmail.com>
2024-09-26 09:05:06 +0100

    Update LocalFileTest.py
    Testing location filter

--- LocalFileTest.py
+++ LocalFileTest.py
@@ -48,7 +48,7 @@ if hit_query :
         hit = check_rows(hit,"Number", 3)
         limb = "Arm"
     elif hit_query < 70:
-        hit = hit.loc[hit["Number"].isin(number_options)]
+        hit = hit.loc[hit[4].isin("Number")]
         limb = "Body"
     elif hit_query < 85:
         hit = check_rows(hit,"Number", 5)
