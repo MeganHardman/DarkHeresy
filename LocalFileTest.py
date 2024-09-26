1b8d2a95085287f1c2476949f211db0219735e76
Megan Hardman <meganlillyhardman@gmail.com>
2024-09-26 11:39:43 +0100

    Update LocalFileTest.py
    Testing!!

--- LocalFileTest.py
+++ LocalFileTest.py
@@ -48,7 +48,7 @@ if hit_query :
         hit = check_rows(hit,"Number", 3)
         limb = "Arm"
     elif hit_query < 70:
-        hit = hit.loc[hit[4].isin("Number")]
+        hit = hit.loc[hit[4].isin(number_options)]
         limb = "Body"
     elif hit_query < 85:
         hit = check_rows(hit,"Number", 5)
