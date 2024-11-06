cb00c041a1e522220a52d29eabf0b68b8b11b1bd
Megan Hardman <meganlillyhardman@gmail.com>
2024-11-06 11:09:29 +0000

    Update LocalFileTest.py
    Switch to checkbox

--- LocalFileTest.py
+++ LocalFileTest.py
@@ -109,10 +109,10 @@ with tab2:
     def SR(): 
         global mod
         mod += 20
+        st.write(mod)
     
     st.header("Attack Modifiers")
-    st.write(mod)
-    st.button("Short Range", key=1, help="+20", on_click= SR, args=None, kwargs=None, type="secondary", icon=None, disabled=False, use_container_width=False)
+    st.checkbox("Short Range", key=1, help="+20", on_click= SR, args=None, kwargs=None, type="secondary", icon=None, disabled=False, use_container_width=False)
     
     
 with tab3:
